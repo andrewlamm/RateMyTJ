058a96817032ddd8658c0171bc2f83711d3e4439
superandybean <andrewlamm04@gmail.com>
2021-10-18 15:44:54 -0400

    add alt names

--- classes.py
+++ classes.py
@@ -78,7 +78,7 @@ for i in range(len(class_id_list)):
 			curr_ind = 0
 			curr_typ = 0
 
-	class_dict[class_id_list[i]] = [classes[curr_cat][curr_typ][curr_ind], num_to_category[curr_cat], "Core" if curr_typ == 0 else "Elective", class_credit_len[i], class_credit_weight[i]]
+	class_dict[class_id_list[i]] = [classes[curr_cat][curr_typ][curr_ind], "", num_to_category[curr_cat], "Core" if curr_typ == 0 else "Elective", class_credit_len[i], class_credit_weight[i]]
 
 	for j in range(len(STATS)):
 		s = str(round(random.random() * 10, 2))
@@ -88,6 +88,38 @@ for i in range(len(class_id_list)):
 
 	curr_ind += 1
 print(class_dict)
+
+# add alternate names
+class_dict["9828T3"][1] = "Engineering Design Research"
+class_dict["122000"][1] = "Broadcast Journalism 1"
+class_dict["122012"][1] = "Broadcast Journalism 2"
+class_dict["122013"][1] = "Broadcast Journalism 3"
+class_dict["122014"][1] = "Broadcast Journalism 4"
+class_dict["612000"][1] = "epf"
+class_dict["612036"][1] = "epf"
+class_dict["3190T1"][1] = "RS1"
+class_dict["317861"][1] = "diffeq"
+class_dict["318561"][1] = "AP CS"
+class_dict["319800"][1] = "Linear Alegbra"
+class_dict["3184T1"][1] = "Foundations CS"
+class_dict["3184T2"][1] = "Accelerated foundations CS"
+class_dict["3199T6"][1] = "ML1"
+class_dict["3199T7"][1] = "ML2"
+class_dict["3190T3"][1] = "RS3"
+class_dict["319862"][1] = "AMT"
+class_dict["319917"][1] = "CV2"
+class_dict["3190T2"][1] = "RS2"
+class_dict["319916"][1] = "CV"
+class_dict["319966"][1] = "AI1"
+class_dict["319967"][1] = "AI2"
+class_dict["3199T3"][1] = "syslab"
+class_dict["3199R1"][1] = "mobile web"
+class_dict["4520T9"][1] = "Quantum Mechanics"
+class_dict["2221T1"][1] = "world history geography"
+class_dict["2360T1"][1] = "USVA"
+class_dict["231905"][1] = "APUSH"
+class_dict["2340T1"][1] = "20th century"
+class_dict["280404"][1] = "AP Econ Macro micro"
 
 # file = open("tableout.txt", "w")
 
@@ -106,16 +138,17 @@ file.write('\t"classes": [\n')
 for class_id in class_dict:
 	file.write('\t\t{\n')
 	file.write('\t\t\t"className": "' + class_dict[class_id][0] + '",\n')
+	file.write('\t\t\t"alternate": "' + class_dict[class_id][1] + '",\n')
 	file.write('\t\t\t"ID": "' + class_id + '",\n')
-	file.write('\t\t\t"Category": "' + class_dict[class_id][1] + '",\n')
-	file.write('\t\t\t"Type": "' + class_dict[class_id][2] + '",\n')
-	file.write('\t\t\t"Length": "' + class_dict[class_id][3] + '",\n')
-	file.write('\t\t\t"Weight": "' + class_dict[class_id][4] + '",\n')
+	file.write('\t\t\t"category": "' + class_dict[class_id][2] + '",\n')
+	file.write('\t\t\t"type": "' + class_dict[class_id][3] + '",\n')
+	file.write('\t\t\t"length": "' + class_dict[class_id][4] + '",\n')
+	file.write('\t\t\t"weight": "' + class_dict[class_id][5] + '",\n')
 	for i in range(len(STATS)):
 		if i == len(STATS)-1:
-			file.write('\t\t\t"' + STATS[i] + '": "' + class_dict[class_id][5+i] + '"\n')
+			file.write('\t\t\t"' + STATS[i] + '": "' + class_dict[class_id][6+i] + '"\n')
 		else:
-			file.write('\t\t\t"' + STATS[i] + '": "' + class_dict[class_id][5+i] + '",\n')
+			file.write('\t\t\t"' + STATS[i] + '": "' + class_dict[class_id][6+i] + '",\n')
 	file.write('\t\t},\n')
 file.write('\t],\n')
 file.write("}\n")
@@ -129,16 +162,17 @@ s += ('"classes": [')
 for class_id in class_dict:
 	s += ('{')
 	s += ('"className": "' + class_dict[class_id][0] + '",')
+	s += ('"alternate": "' + class_dict[class_id][1] + '",')
 	s += ('"ID": "' + class_id + '",')
-	s += ('"Category": "' + class_dict[class_id][1] + '",')
-	s += ('"Type": "' + class_dict[class_id][2] + '",')
-	s += ('"Length": "' + class_dict[class_id][3] + '",')
-	s += ('"Weight": "' + class_dict[class_id][4] + '",')
+	s += ('"category": "' + class_dict[class_id][2] + '",')
+	s += ('"type": "' + class_dict[class_id][3] + '",')
+	s += ('"length": "' + class_dict[class_id][4] + '",')
+	s += ('"weight": "' + class_dict[class_id][5] + '",')
 	for i in range(len(STATS)):
 		if i == len(STATS)-1:
-			s += ('"' + STATS[i] + '": "' + class_dict[class_id][5+i] + '"')
+			s += ('"' + STATS[i] + '": "' + class_dict[class_id][6+i] + '"')
 		else:
-			s += ('"' + STATS[i] + '": "' + class_dict[class_id][5+i] + '",')
+			s += ('"' + STATS[i] + '": "' + class_dict[class_id][6+i] + '",')
 	s += ('},')
 s += ('],')
 s += ("}")
